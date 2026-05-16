0278732ed8510cc246be95176a1da78438b8bf9b
ngsmhr2 <ngsmhr2@users.noreply.github.com>
2026-05-17 06:01:43 +0900

    ToDoリスト

---
+++ js/ToDolist.js
@@ -0,0 +1,66 @@
+/*
+window.addEventListener('load', () => {
+
+});
+*/
+
+const form = document.querySelector("#form");
+
+const list = document.querySelector("#list");
+
+const deleteButton = document.querySelector("#deleteButton");
+
+
+/* ToDo追加 */
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const input = document.querySelector("#text");
+
+    const li = document.createElement("li");
+
+    /* チェックボックス */
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    /* 文字 */
+    const span = document.createElement("span");
+    span.textContent = input.value;
+
+    /* チェック時 */
+    checkbox.addEventListener("change", () => {
+
+        span.classList.toggle("completed");
+
+    });
+
+    li.appendChild(span);
+
+    li.appendChild(checkbox);
+
+    list.appendChild(li);
+
+    input.value = '';
+
+});
+
+
+/* チェック済み削除 */
+deleteButton.addEventListener("click", () => {
+
+    const items = document.querySelectorAll("li");
+
+    items.forEach((item) => {
+
+        const checkbox = item.querySelector("input");
+
+        if (checkbox.checked) {
+
+            item.remove();
+
+        }
+
+    });
+
+});
