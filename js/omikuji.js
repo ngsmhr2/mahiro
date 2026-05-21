f6bc297808dfd9c133a423a735ba22b1516e7e21
ngsmhr2 <ngsmhr2@users.noreply.github.com>
2026-05-22 05:57:47 +0900

    おみくじアップデート

--- js/omikuji.js
+++ js/omikuji.js
@@ -3,7 +3,7 @@
 function omikuji() {
 
     //りざると
-    const results = ["大吉", "中吉", "小吉", "凶"];
+    const results = ["大吉", "中吉", "小吉", "凶", "大凶", "死"];
 
     /*
     ますふろあhttps://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
@@ -16,5 +16,23 @@ function omikuji() {
     //ここで0～3に対応するリザルトをだす
 
     document.getElementById("result").textContent = results[random]
+
+}
+
+
+//ひとことコメント
+function todayComment() {
+
+
+    const comment = ["空を見上げると緑色の空にクレヨンみたいに赤い太陽が浮かんでるかも！",
+        "ソシャゲに課金しよう！", "いま殺してしまった虫には感情と思考能力があり、それを伝える手段がなかっただけかも！",
+        "いま、あなたの背中にほくろがひとつ増えた！",
+        "モバイルバッテリーの発火に気を付けよう！"];
+
+
+    const hitokoto = Math.floor(Math.random() * comment.length);
+
+
+    document.getElementById("hitokoto").textContent = comment[hitokoto]
 
 }
